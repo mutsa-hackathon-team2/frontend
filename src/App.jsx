a3537d21550217c5c6c5878a9851aa639a33b42b
KwonOjin <ojin630@naver.com>
2026-07-23 11:46:32 +0900

    [FEAT] 상세 정보 페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router";
 import Main from "./pages/Main.jsx";
+import Detail from "./pages/Detail.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
+      <Route path="/detail" element={<Detail />} />
     </Routes>
   );
 }
---
+++ src/pages/Detail.jsx
@@ -0,0 +1,9 @@
+function Detail() {
+  return (
+    <main>
+      <h1 className="text-heading">상세 화면</h1>
+    </main>
+  );
+}
+
+export default Detail;
