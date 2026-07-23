f31cd2eece06064407979d37b54cea5268db3c96
LEE SANGBIN <novelsb72@naver.com>
2026-07-23 17:11:20 +0900

    [Feat] 카카오 로그인 연동 완료

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,17 @@
 import { Link } from "react-router";
 import profileIcon from "../assets/icons/profile.svg";
-import logoIcon from "../assets/icons/logo.svg";
+import { handleKakaoLogin } from "../utils/kakaoLogin";
 
 function Header() {
   return (
     <header className="flex w-full items-center justify-between px-12 py-5 bg-[#F1F1F1] rounded-100">
-      <Link to="/" className="flex items-center">
-        <img src={logoIcon} alt="" className="h-6 w-26" />
+      <Link to="/" className="flex items-center text-heading">
+      UniCul
       </Link>
 
       <button
         type="button"
+        onClick={handleKakaoLogin}
         className="flex cursor-pointer items-center gap-2 rounded-100 border-0 bg-white px-5 py-2"
       >
         <img src={profileIcon} alt="" className="size-6" />
