ec910647289a25b83aad8f02521e3114cc526462
LEE SANGBIN <novelsb72@naver.com>
2026-07-23 12:09:36 +0900

    상세페이지 초안 작업

---
+++ src/src/data/mockEvents.js
@@ -0,0 +1,15 @@
+export const mockEvents = [
+  {
+    id: 1,
+    title: "데일리파티 여름 버스킹",
+    category: "공연",
+    place: "망원한강공원 내 서울함공원",
+    date: "8.10 ~ 8.12",
+    time: "17:00 ~ 19:00",
+    price: 5000,
+    isFree: false,
+    organizerName: "",
+    imageUrl: "",
+    description: "상세설명텍스트 상세설명텍스트 상세설명텍스트...",
+  },
+];
