dcce2de557660fbfee07be19dd6d3ad9af684432
KwonOjin <ojin630@naver.com>
2026-07-23 12:38:50 +0900

    [FEAT] 헤더 컴포넌트 구현

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,20 +1,26 @@
 import { useNavigate } from "react-router";
+import Header from "../components/Header.jsx";
 
 function Main() {
   const navigate = useNavigate();
 
   return (
-    <main>
-      <h1 className="text-heading">홈화면~~</h1>
-      <p className="text-body2">body2</p>
-      <button
-        type="button"
-        className="cursor-pointer"
-        onClick={() => navigate("/detail")}
-      >
-        상세 페이지로 이동
-      </button>
-    </main>
+    <div>
+      <div className="flex w-full flex-col items-start px-25 pt-5">
+        <Header />
+      </div>
+      <main>
+        <h1 className="text-heading">홈화면~~</h1>
+        <p className="text-body2">body2</p>
+        <button
+          type="button"
+          className="cursor-pointer"
+          onClick={() => navigate("/detail")}
+        >
+          상세 페이지로 이동
+        </button>
+      </main>
+    </div>
   );
 }
 
